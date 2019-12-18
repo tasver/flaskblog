a4fdb005f25c872fc89ff2e133f3240fbf4f40ad
Tasver <bohdannavrotskyi@gmail.com>
2019-12-18 18:31:19 +0200

    change comm14

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flaskblog import db, login_manager
+from flaskblog.app import db, login_manager
 from flask_login import UserMixin
 from hashlib import md5
 from flask import Flask, request, jsonify, make_response
