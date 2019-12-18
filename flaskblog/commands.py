4c2ec54e598c8409198f538992d319713cdca348
Tasver <bohdannavrotskyi@gmail.com>
2019-12-18 17:16:55 +0200

    change comm1

--- flaskblog/commands.py
+++ flaskblog/commands.py
@@ -9,25 +9,4 @@ from .models import User, Post
 def create_tables():
     db.create_all()
 
-@click.group()
-def cli():
-    pass
 
-@click.command(name='create_admin')
-@with_appcontext
-def create_admin():
-    click.echo('Hello! Run command ok before')
-    nickname = "superadmin"
-    admin = True
-    u = User(nickname=nickname, admin=admin)
-    #print("superadmin created", u)
-    db.session.add(u)
-    db.session.commit()
-    click.echo("superadmin created!!!")
-
-cli.add_command(create_tables)
-cli.add_command(create_admin)
-
-
-if __name__ == '__main__':
-    cli()
